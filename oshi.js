27a0835f7ddedd32bb6cde9d619bf0428c1f4a12
Eleni Lixourioti <inelegr88@gmail.com>
2011-09-17 22:30:10 +0000

    piece creation

--- oshi.js
+++ oshi.js
@@ -8,8 +8,17 @@ var Oshi = {
     piece: {
         width: 44.4,
         height: 44.4,
-        red_image: 'images/red.jpg',
-        yellow_image: 'images/yellow.jpg'
+        red_image: 'http://www.useful-free-stuff.com/Data/icons/simple-red-square-icon-symbols-shapes-full-set/simple-red-square-icon-symbols-shapes-shape-square-frame.png',
+        yellow_image: 'http://www.snowmobilestud.com/shop/store/20100910001/items/thumbnails/yellow-square-aluminum-backer.jpg',
+        draw: function(color, x, y) {
+            Oshi.canvas.canvas_el.drawImage({
+                source: this[color+'_image'],
+                x: x + this.width / 2,
+                y: y + this.height / 2,
+                width: this.width - 2,
+                height: this.height - 2
+            });
+        }
     },
     canvas: {
         canvas_width: 400,
@@ -53,6 +62,29 @@ var Oshi = {
                     x2: this.canvas_width,  y2: y
                 });
             }
+            
+            Oshi.piece.draw('red', 0, 0);
+            Oshi.piece.draw('red', Oshi.piece.width * 8, 0);
+            
+            Oshi.piece.draw('red', Oshi.piece.width * 2, Oshi.piece.height*1);
+            Oshi.piece.draw('red', Oshi.piece.width * 3, Oshi.piece.height*1);
+            Oshi.piece.draw('red', Oshi.piece.width * 4, Oshi.piece.height*1);
+            Oshi.piece.draw('red', Oshi.piece.width * 5, Oshi.piece.height*1);
+            Oshi.piece.draw('red', Oshi.piece.width * 6, Oshi.piece.height*1);
+            
+            Oshi.piece.draw('red', Oshi.piece.width * 4, Oshi.piece.height*2);
+            
+            
+            Oshi.piece.draw('yellow', 0, this.canvas_height-Oshi.piece.height);
+            Oshi.piece.draw('yellow', this.canvas_width - Oshi.piece.width,  this.canvas_height-Oshi.piece.height*1);
+            
+            Oshi.piece.draw('yellow', Oshi.piece.width * 2, this.canvas_height-Oshi.piece.height*2);
+            Oshi.piece.draw('yellow', Oshi.piece.width * 3, this.canvas_height-Oshi.piece.height*2);
+            Oshi.piece.draw('yellow', Oshi.piece.width * 4, this.canvas_height-Oshi.piece.height*2);
+            Oshi.piece.draw('yellow', Oshi.piece.width * 5, this.canvas_height-Oshi.piece.height*2);
+            Oshi.piece.draw('yellow', Oshi.piece.width * 6, this.canvas_height-Oshi.piece.height*2);
+            
+            Oshi.piece.draw('yellow', Oshi.piece.width * 4, this.canvas_height-Oshi.piece.height*3);
         }
     }
 };
